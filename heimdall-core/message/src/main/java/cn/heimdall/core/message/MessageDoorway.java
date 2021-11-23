932c1aedf0cae370ad5373efdcf6c68f7a95380a
e <840789866@qq.com>
2021-11-23 21:08:49 +0800

    add spi loader and modify message request

--- heimdall-core/message/src/main/java/cn/heimdall/core/message/MessageDoorway.java
+++ heimdall-core/message/src/main/java/cn/heimdall/core/message/MessageDoorway.java
@@ -5,7 +5,7 @@ import cn.heimdall.core.message.MessageBody;
 /**
  * 通用的消息周期
  */
-public interface MessageTransaction {
+public interface OnRequest {
     //处理消息的请求
     MessageBody onRequest(MessageBody request);
     //处理消息的返回
