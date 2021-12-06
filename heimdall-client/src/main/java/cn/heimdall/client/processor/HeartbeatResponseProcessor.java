daac218b6f0914030066cc6b1ef87d8691f26ac3
e <840789866@qq.com>
2021-12-06 20:58:49 +0800

    debug client register

--- heimdall-client/src/main/java/cn/heimdall/client/processor/HeartbeatResponseProcessor.java
+++ heimdall-client/src/main/java/cn/heimdall/client/processor/HeartbeatResponseProcessor.java
@@ -2,11 +2,32 @@ package cn.heimdall.client.processor;
 
 import cn.heimdall.core.message.Message;
 import cn.heimdall.core.network.processor.ClientProcessor;
+import cn.heimdall.core.network.remote.MessageFuture;
 import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class HeartbeatResponseProcessor implements ClientProcessor {
+import java.util.concurrent.ConcurrentMap;
+
+public class ClientCommonResponseProcessor implements ClientProcessor {
+
+    private ConcurrentMap<Integer, MessageFuture> futures;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientCommonResponseProcessor.class);
+
+    public ClientCommonResponseProcessor(ConcurrentMap<Integer, MessageFuture> futures) {
+        this.futures = futures;
+    }
+
     @Override
     public void process(ChannelHandlerContext ctx, Message message) {
-
+        MessageFuture messageFuture = futures.remove(message.getMessageId());
+        if (messageFuture != null) {
+            messageFuture.setResultMessage(message.getMessageBody());
+        } else {
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info("the client received response msg [{}] from tc guarder server.", message.toString());
+            }
+        }
     }
 }
